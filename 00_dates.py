5d9dda468d89156fd0fe635caf6c633d454cb761
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-10 20:53:10 -0300

    Revisando el objeto timestamp

--- 00_dates.py
+++ 00_dates.py
@@ -10,3 +10,7 @@ print(now.day)
 print(now.hour)
 print(now.minute)
 print(now.second)
+
+timestamp = now.timestamp() # Se toma como medida de tiempo el número de segundos transcurridos desde el primero de enero de 1970 a las 0 horas UTC hasta el momento a representar
+
+print(timestamp)
