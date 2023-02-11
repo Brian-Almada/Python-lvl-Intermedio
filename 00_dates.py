6aea1f4e5e7ddb48976fb8437883b2637741c315
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-10 22:49:45 -0300

    Corrigiendo

--- 00_dates.py
+++ 00_dates.py
@@ -4,13 +4,19 @@ from datetime import datetime
 
 now = datetime.now()
 
-print(now.year)
-print(now.month)
-print(now.day)
-print(now.hour)
-print(now.minute)
-print(now.second)
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+
+print_date(now)
 
 timestamp = now.timestamp() # Se toma como medida de tiempo el número de segundos transcurridos desde el primero de enero de 1970 a las 0 horas UTC hasta el momento a representar
 
 print(timestamp)
+
+year_2023 = datetime(2023, 1, 22)
+
