fa2de75936f299f18153b3fb6836d69b64b5871c
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-13 22:38:09 -0300

    Probando funciones lambda

--- 03_lambdas.py
+++ 03_lambdas.py
@@ -9,3 +9,22 @@ print(sum_two_values(2, 4))
 
 multiply_values = lambda first_value, second_value: first_value * second_value - 3
 print(multiply_values(2, 4))
+
+# Funciones que retornan lambdas:
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+print(sum_three_values(5)(2, 4))
+
+# Solo es necesario una función lambada y se puede retornar en 3 operaciones distintas
+def multiplicar_por(n):
+    return lambda x: x * n
+
+duplicar = multiplicar_por(2)
+triplicar = multiplicar_por(3)
+diez_veces = multiplicar_por(10)
+
+print(duplicar(6))
+print(triplicar(2))
+print(diez_veces(1000))
