b89862ebbf5d2dc9acf65167d4f43a9eaa6c6ebf
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-14 21:38:46 -0300

    Probando función que recive una función como parámetro

--- 04_higher_order_functions.py
+++ 04_higher_order_functions.py
@@ -8,3 +8,16 @@ def sum_two_values_and_add_one(first_value, second_value):
     return sum_one(first_value + second_value)
 
 print(sum_two_values_and_add_one(5, 2))
+
+
+def sum_one(value):
+    return value + 1
+
+def sum_five(value):
+    return value + 5
+
+def sum_two_values_and_add_value(first_value, second_value, f_sum):
+    return f_sum(first_value + second_value)
+
+print(sum_two_values_and_add_value(5, 2, sum_one))
+print(sum_two_values_and_add_value(5, 2, sum_five))
