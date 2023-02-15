55e37beb1e63a109d41ac8846979d0195d7e4929
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-14 21:12:04 -0300

    Probando función que retorna una función

--- 04_higher_order_functions.py
+++ 04_higher_order_functions.py
@@ -1,1 +1,10 @@
 # Higher Order Functions
+# Una función se denomina Función de orden superior si esta contiene otras funciones como parámetros de entrada o si devuelve una función como salida
+
+def sum_one(value):
+    return value + 1
+
+def sum_two_values_and_add_one(first_value, second_value):
+    return sum_one(first_value + second_value)
+
+print(sum_two_values_and_add_one(5, 2))
