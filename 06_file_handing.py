fbafdf0d19bb8d62384599b954a4491ad858dd22
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-17 21:31:10 -0300

    Creando y borrando archivos

--- 06_file_handing.py
+++ 06_file_handing.py
@@ -1,1 +1,22 @@
 # File Handling
+
+# .txt file
+import os
+
+txt_files = open("./my_files.txt", "w+")
+
+txt_files.write("Mi nombre es Brian\nMi apellido Almada\n 32 años\nY mi lenguaje favorito es Python")
+
+
+#print(txt_file.read())
+#print(txt_file.read(10))
+#print(txt_file.readline())
+#print(txt_file.readlines())
+#for line in txt_file.readlines():
+    #print(line)
+
+txt_files.write("\nAunque también me gusta JavaScript")
+print(txt_files.readlines())
+
+txt_files.close()
+os.remove("./my_files.txt")
