da8cb696e202358a222791b3a09264a1afff9031
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-20 19:58:23 -0300

    Abriendo y creando archivo json

--- 06_file_handing.py
+++ 06_file_handing.py
@@ -3,9 +3,9 @@
 # .txt file
 import os
 
-txt_files = open("./my_files.txt", "w+")
+#txt_files = open("./my_files.txt", "w+")
 
-txt_files.write("Mi nombre es Brian\nMi apellido Almada\n 32 años\nY mi lenguaje favorito es Python")
+#txt_files.write("Mi nombre es Brian\nMi apellido Almada\n 32 años\nY mi lenguaje favorito es Python")
 
 
 #print(txt_file.read())
@@ -15,8 +15,21 @@ txt_files.write("Mi nombre es Brian\nMi apellido Almada\n 32 años\nY mi lenguaj
 #for line in txt_file.readlines():
     #print(line)
 
-txt_files.write("\nAunque también me gusta JavaScript")
-print(txt_files.readlines())
+#txt_files.write("\nAunque también me gusta JavaScript")
+#print(txt_files.readlines())
 
-txt_files.close()
-os.remove("./my_files.txt")
+#txt_files.close()
+#os.remove("./my_files.txt")
+
+# .json file
+
+import json
+
+json_file = open("./my_file.json", "w+")
+
+json_text = {
+    "name":"Brian",
+    "surname":"Almada",
+    "age": 32,
+    "lenguge":"Python"
+}
