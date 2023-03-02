0e0276013c59f275783a6ecd74d076d0da1f2aac
Brian Almada <brianalexisalmada84@gmail.com>
2023-03-01 22:38:09 -0300

    Usando el objero match en expresiones regulares

--- 07_regular_expressions.py
+++ 07_regular_expressions.py
@@ -1,1 +1,23 @@
 # Regular Expressions
+
+import re
+
+my_string = "Esta es la lección número 7: Expresiones Regulares"
+
+my_other_string = "Esta no es la lección número 6: Manejo de ficheros"
+
+print(re.match("Esta es la lección", my_string)) # match reconoce si hay algún patrón en el string
+print(re.match("Esta es la lección", my_other_string))# Aquí nos dice que esta porción de texto no se encuentra en la variable my_other_string
+print(re.match("Expresiones Regulares", my_string)) # Dice none porque match comienza a buscar desde el principio.
+
+match = re.match("Esta es la lección", my_string, re.I)
+print(match)
+start, end = match.span()
+print(my_string[start:end])
+
+match = print(re.match("Esta es la lección", my_other_string))
+if not(match == None):
+    print(match)
+    start, end = match.span()
+    print(my_string[start:end])
+
