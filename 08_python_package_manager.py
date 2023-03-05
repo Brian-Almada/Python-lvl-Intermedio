337ccad761061ea4b14b941534cb8e95edadf703
Brian Almada <brianalexisalmada84@gmail.com>
2023-03-05 09:51:32 -0300

    Agregando comentario teórico sobre pip

--- 08_python_package_manager.py
+++ 08_python_package_manager.py
@@ -1,1 +1,3 @@
 # Python Package Manager
+
+# pip es el gestor de paquetes de Python. Es una herramienta que permite buscar, instalar, actualizar y desinstalar paquetes o módulos de Python en nuestro sistema.
