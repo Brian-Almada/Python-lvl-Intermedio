879796ecb0ef98b22d5740bd53c4a9d77c29e0d6
Brian Almada <brianalexisalmada84@gmail.com>
2023-03-05 10:02:15 -0300

    Descargando y usando librería NumPy

--- 08_python_package_manager.py
+++ 08_python_package_manager.py
@@ -1,3 +1,12 @@
 # Python Package Manager
 
 # pip es el gestor de paquetes de Python. Es una herramienta que permite buscar, instalar, actualizar y desinstalar paquetes o módulos de Python en nuestro sistema.
+
+import numpy
+
+print(numpy.version.version)
+
+numpy_array = numpy.array([54,54,895,85,632,6898,3])
+print(type(numpy_array))
+
+print(numpy_array * 2)
